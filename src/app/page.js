704ad03fa9b049eeb80e88d4a35001b2f2bb3b9c
Adam Prchal <prchaladam@outlook.cz>
2023-06-07 23:01:12 +0200

    init

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import HelloWorld from "../chapters/hello.mdx";
+
+export default function Home() {
+  return (
+    <main className="container">
+      <h1>Predikce hodnocení hry na základě různých parametrů</h1>
+      <article>
+        <HelloWorld />
+      </article>
+    </main>
+  );
+}
