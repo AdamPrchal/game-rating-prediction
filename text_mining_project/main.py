39c2e3a9f162e5d4a5cd5d509a060aa609f0a454
tomaslacina <73538659+tomaslacina@users.noreply.github.com>
2024-06-05 14:46:43 +0200

    Basic data analysiss and visualization - most frequently used words displayed in a word cloud graph

--- text_mining_project/main.py
+++ text_mining_project/main.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import nltk
 import string
-
+import matplotlib
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from collections import Counter
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+matplotlib.use('TkAgg')
 
 
 nltk.download('punkt')
@@ -29,6 +34,14 @@ def preprocess_text(text):
     else:
         return ''
 
+#funkce pro vizualizaci dat pomocí word cloud (slovní mraky)
+def plot_word_cloud(text, title):
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
 
 
 top_game_ids = pd.read_csv('./data/top_game_app_ids.csv')
@@ -60,13 +73,22 @@ worst_game_reviews = worst_game_reviews.merge(worst_game_ids, on='app_id', how='
 #print(worst_game_reviews.head())
 
 
-# Aplikace předzpracování na recenze
+# Předzpracování recenzí pomocí funkce preprocess_text()
 top_game_reviews['cleaned_review'] = top_game_reviews['review'].apply(preprocess_text)
 worst_game_reviews['cleaned_review'] = worst_game_reviews['review'].apply(preprocess_text)
 
 ## Výpis předzpracovaných dat
-print("\nPreprocessed Top Game Reviews:")
-print(top_game_reviews[['review', 'cleaned_review']].head())
-print("\nPreprocessed Worst Game Reviews:")
-print(worst_game_reviews[['review', 'cleaned_review']].head())
+#print("\nPreprocessed Top Game Reviews:")
+#print(top_game_reviews[['review', 'cleaned_review']].head())
+#print("\nPreprocessed Worst Game Reviews:")
+#print(worst_game_reviews[['review', 'cleaned_review']].head())
+
+
+# Spojení všech recenzí do jednoho velkého textu, jednotlivé recenze jsou odděleny mezerou
+top_reviews_text = ' '.join(top_game_reviews['cleaned_review'])
+worst_reviews_text = ' '.join(worst_game_reviews['cleaned_review'])
+
+# Vykreslení grafu word clouds
+plot_word_cloud(top_reviews_text, 'Top Game Reviews Word Cloud')
+plot_word_cloud(worst_reviews_text, 'Worst Game Reviews Word Cloud')
 
