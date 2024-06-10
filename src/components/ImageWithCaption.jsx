f2e6f795fa2a8df9b58182ee9a0db891dcbfcafa
Adam Prchal <adamprchal@AdamovahaProfik.lan>
2024-06-10 08:15:58 +0200

    add image link

--- src/components/ImageWithCaption.jsx
+++ src/components/ImageWithCaption.jsx
@@ -5,7 +5,9 @@ import React from "react";
 export const ImageWithCaption = ({ src, caption, source, lang}) => {
   return (
     <figure>
-      <img src={src} alt={caption} />
+      <a href={src} target="_blank" className="not-prose">
+       <img src={src} alt={caption} />
+      </a>
       <figcaption style={{fontSize: "0.75rem"}}>
         {caption} {source && <a href={source}>{lang === "cs" ? "Zdroj" : "Source"}</a>}
       </figcaption>
